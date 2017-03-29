141c959cfdc8d3e7265b4afe2120652000bbb91e
Amin Zare <amin.zare.spo16@ecusolna.onmicrosoft.com>
2017-03-29 17:09:31 +0200

    weather after four dayes indefault page

--- weather/Pages/Default.aspx
+++ weather/Pages/Default.aspx
@@ -1,56 +1,43 @@
-﻿<%-- The following 4 lines are ASP.NET directives needed when using SharePoint components --%>
-
-<%@ Page Inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" MasterPageFile="~masterurl/default.master" Language="C#" %>
-
+﻿<%@ Page Inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" MasterPageFile="~masterurl/default.master" Language="C#" %>
 <%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
 <%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
 <%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
-
-<%-- The markup and script in the following Content element will be placed in the <head> of the page --%>
 <asp:Content ContentPlaceHolderID="PlaceHolderAdditionalPageHead" runat="server">
 		<script type="text/javascript" src="../Scripts/jquery-3.1.1.min.js"></script>
     <SharePoint:ScriptLink name="sp.js" runat="server" OnDemand="true" LoadAfterUI="true" Localizable="false" />
     <meta name="WebPartPageExpansion" content="full" />
-
     <!-- Add your CSS styles to the following file -->
+	<link href="../Content/bootstrap.min.css" rel="stylesheet" />
 	<link href="../Content/App.css" rel="stylesheet" />
     <!-- Add your JavaScript to the following file -->
-
-	<script src="../Scripts/moment.js"></script>
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/moment.min.js"></script>
+	<script src="../Scripts/jquery-3.1.1.min.js"></script>
+	<script src="../Scripts/jQueryUI.js"></script>
+	<script src="../Scripts/jquery.dataTables.min.js"></script>
+	<script src="../Scripts/moment.min.js"></script>
+	<script src="../Scripts/moment-with-locales.min.js"></script>
+	<script src="../Scripts/moment-with-locales.js"></script>
+	<script src="../Scripts/bootstrap.min.js"></script>
+	<script src="../Scripts/skycons.js"></script>
 </asp:Content>
-
-<%-- The markup in the following Content element will be placed in the TitleArea of the page --%>
 <asp:Content ContentPlaceHolderID="PlaceHolderPageTitleInTitleArea" runat="server">
-    Page Title
 </asp:Content>
-
-<%-- The markup and script in the following Content element will be placed in the <body> of the page --%>
 <asp:Content ContentPlaceHolderID="PlaceHolderMain" runat="server">
 <script type="text/javascript">
-
 	var hostweburl;
 	var appweburl;
-
-	// Load the required SharePoint libraries
 	$(document).ready(function () {
 		//Get the URI decoded URLs.
 		hostweburl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
 		appweburl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
-
 		// resources are in URLs in the form: web_url/_layouts/15/resource
 		var scriptbase = hostweburl + "/_layouts/15/";
-
 		// Load the js files and continue to the successHandler
 		$.getScript(scriptbase + "SP.RequestExecutor.js", execCrossDomainRequest);
-
 	});
-
 	// Function to prepare and issue the request to get SharePoint data
 	function execCrossDomainRequest() {
 		// executor: The RequestExecutor object Initialize the RequestExecutor with the app web URL.
 		var executor = new SP.RequestExecutor(appweburl);
-
 		// Deals with the issue the call against the app web.
 		executor.executeAsync({
 			url: appweburl + "/_api/SP.AppContextSite(@target)/web/lists/getbytitle('AppEvents')/items?@target='" + hostweburl + "'&$top=5",
@@ -76,22 +63,15 @@
 				"<div id='" + "kdate" + "' href=\"" + hostweburl + "/blog/Lists/Posts/Post.aspx?ID=" + this.ID + "\" target=\"_blank\">" + formattedEventDate + "</div>" +
 				"<div id='" + "kDes" + "' href=\"" + hostweburl + "/blog/Lists/Posts/Post.aspx?ID=" + this.ID + "\" target=\"_blank\">" + this.Description + "</div>" +
 
-				'</li>');
-			
-			
+				'</li>');			
 		});
-
 		items.push("</ul");
 		$("#listResult").html(items.join(''))
-
 	}
-
-
 	// Function to handle the error event. Prints the error message to the page.
 	function errorHandler(data, errorCode, errorMessage) {
 		document.getElementById("internal").innerText = "Could not complete cross-domain call: " + errorMessage;
 	}
-
 	// Function to retrieve a query string value.
 	function getQueryStringParameter(paramToRetrieve) {
 		var params =
@@ -103,12 +83,42 @@
 				return singleParam[1];
 		}
 	}
-	
     </script>
+	<div class="container ">
+  <div class="row ">
+    <div class="col-sm-2">
+		<h4 id="toMar"></h4>
+      <h3> <canvas id="dicon0" width="60" height="60"></canvas></h3>
+		<p id="dmTempo0"></p>
+      <p id="dminTempo0"></p>
+      <p id="dsumaRy0"></p>
+    </div>
+    <div class="col-sm-2">
+		<h4 id="otoMar"></h4>
+      <h3> <canvas id="dicon1" width="60" height="60"></canvas></h3>
+		<p id="dmTempo1"></p>
+      <p id="dminTempo1"></p>
+      <p id="dsumaRy1"></p>
+    </div>
+    <div class="col-sm-2 ">
+		<h4 id="totoMar"></h4>
+      <h3> <canvas id="dicon2" width="60" height="60"></canvas></h3>
+		<p id="dmTempo2"></p>
+      <p id="dminTempo2"></p>
+      <p id="dsumaRy2"></p>
+    </div>
+	   <div class="col-sm-2 ">
+		   <h4 id="tretoMar"></h4>
+	  <h3> <canvas id="dicon3" width="60" height="60"></canvas></h3>
+		<p id="dmTempo3"></p>
+      <p id="dminTempo3"></p>
+      <p id="dsumaRy3"></p>
+    </div>
+  </div>
+</div>
+	      
 	  <div><strong>Latest blog posts</strong></div>
       <div id="listResult"></div>
-	<div id="test1"></div>
-	<div id="test2"></div>
-	
+		<script src="../Scripts/App.js"></script>
 	
 </asp:Content>
--- weather/Scripts/App.js
+++ weather/Scripts/App.js
@@ -62,17 +62,275 @@ getLatitudeLongitude(showResult);
 				dataType: 'jsonp',
 				timeout: 3000,
 				success: function (data) {
+					function tommorow(){
+						var icon0 = data.daily.data[0].icon;
+						var mTempo0 = data.daily.data[0].temperatureMax.toFixed(0);	
+						var minTempo0 = data.daily.data[0].temperatureMin.toFixed(0);
+						var sumaRy0 = data.daily.data[0].summary;
+
+						if (icon0 == "clear-day") {
+							var skycons = new Skycons({ "color": "Yellow" });
+							skycons.add("dicon0", Skycons.CLEAR_DAY);
+							skycons.play();
+						}
+						if (icon0 == "clear-night") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon0", Skycons.CLEAR_NIGHT);
+							skycons.play();
+						}
+						if (icon0 == "partly-cloudy-day") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon0", Skycons.PARTLY_CLOUDY_DAY);
+							skycons.play();
+						}
+						if (icon0 == "partly-cloudy-night") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon0", Skycons.PARTLY_CLOUDY_NIGHT);
+							skycons.play();
+						}
+						if (icon0 == "cloudy") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon0", Skycons.CLOUDY);
+							skycons.play();
+						}
+						if (icon0 == "rain") {
+							var skycons = new Skycons({ "color": "blue" });
+							skycons.add("dicon0", Skycons.RAIN);
+							skycons.play();
+						}
+						if (icon0 == "sleet") {
+							var skycons = new Skycons({ "color": "blue" });
+							skycons.add("dicon0", Skycons.SLEET);
+							skycons.play();
+						}
+						if (icon0 == "snow") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon0", Skycons.SNOW);
+							skycons.play();
+						}
+						if (icon0 == "wind") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon0", Skycons.WIND);
+							skycons.play();
+						}
+						if (icon0 == "fog") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon0", Skycons.FOG);
+							skycons.play();
+						}
+						$('#dicon0').html(icon0);
+						$('#dmTempo0').html('Max ' + mTempo0 + '&#8451');
+						$('#dminTempo0').html('Min ' +minTempo0 + '&#8451');
+						$('#dsumaRy0').html(sumaRy0);
+					}
+					tommorow();
+					function afterTommorow() {
+						var icon1 = data.daily.data[1].icon;
+						var mTempo1 = data.daily.data[1].temperatureMax.toFixed(0);
+						var minTempo1 = data.daily.data[1].temperatureMin.toFixed(0);
+						var sumaRy1 = data.daily.data[1].summary;
+
+						if (icon1 == "clear-day") {
+							var skycons = new Skycons({ "color": "Yellow" });
+							skycons.add("dicon1", Skycons.CLEAR_DAY);
+							skycons.play();
+						}
+						if (icon1 == "clear-night") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon1", Skycons.CLEAR_NIGHT);
+							skycons.play();
+						}
+						if (icon1 == "partly-cloudy-day") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon1", Skycons.PARTLY_CLOUDY_DAY);
+							skycons.play();
+						}
+						if (icon1 == "partly-cloudy-night") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon1", Skycons.PARTLY_CLOUDY_NIGHT);
+							skycons.play();
+						}
+						if (icon1 == "cloudy") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon1", Skycons.CLOUDY);
+							skycons.play();
+						}
+						if (icon1 == "rain") {
+							var skycons = new Skycons({ "color": "blue" });
+							skycons.add("dicon1", Skycons.RAIN);
+							skycons.play();
+						}
+						if (icon1 == "sleet") {
+							var skycons = new Skycons({ "color": "blue" });
+							skycons.add("dicon1", Skycons.SLEET);
+							skycons.play();
+						}
+						if (icon1 == "snow") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon1", Skycons.SNOW);
+							skycons.play();
+						}
+						if (icon1 == "wind") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon1", Skycons.WIND);
+							skycons.play();
+						}
+						if (icon1 == "fog") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon1", Skycons.FOG);
+							skycons.play();
+						}
+						$('#dicon1').html(icon1);
+						$('#dmTempo1').html('Max ' + mTempo1 + '&#8451');
+						$('#dminTempo1').html('Min ' + minTempo1 + '&#8451');
+						$('#dsumaRy1').html(sumaRy1);
+					}
+					afterTommorow();
+					function oneAfterTommorow() {
+						var icon2 = data.daily.data[2].icon;
+						var mTempo2 = data.daily.data[2].temperatureMax.toFixed(0);
+						var minTempo2 = data.daily.data[2].temperatureMin.toFixed(0);
+						var sumaRy2 = data.daily.data[2].summary;
+
+						if (icon2 == "clear-day") {
+							var skycons = new Skycons({ "color": "Yellow" });
+							skycons.add("dicon2", Skycons.CLEAR_DAY);
+							skycons.play();
+						}
+						if (icon2 == "clear-night") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon2", Skycons.CLEAR_NIGHT);
+							skycons.play();
+						}
+						if (icon2 == "partly-cloudy-day") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon2", Skycons.PARTLY_CLOUDY_DAY);
+							skycons.play();
+						}
+						if (icon2 == "partly-cloudy-night") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon2", Skycons.PARTLY_CLOUDY_NIGHT);
+							skycons.play();
+						}
+						if (icon2 == "cloudy") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon2", Skycons.CLOUDY);
+							skycons.play();
+						}
+						if (icon2 == "rain") {
+							var skycons = new Skycons({ "color": "blue" });
+							skycons.add("dicon2", Skycons.RAIN);
+							skycons.play();
+						}
+						if (icon2 == "sleet") {
+							var skycons = new Skycons({ "color": "blue" });
+							skycons.add("dicon2", Skycons.SLEET);
+							skycons.play();
+						}
+						if (icon2 == "snow") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon2", Skycons.SNOW);
+							skycons.play();
+						}
+						if (icon2 == "wind") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon2", Skycons.WIND);
+							skycons.play();
+						}
+						if (icon2 == "fog") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon2", Skycons.FOG);
+							skycons.play();
+						}
+						$('#dicon2').html(icon2);
+						$('#dmTempo2').html('Max ' + mTempo2 + '&#8451');
+						$('#dminTempo2').html('Min ' + minTempo2 + '&#8451');
+						$('#dsumaRy2').html(sumaRy2);
+					}
+					oneAfterTommorow();
+					function twoAfterTommorow() {
+						var icon3 = data.daily.data[3].icon;
+						var mTempo3 = data.daily.data[3].temperatureMax.toFixed(0);
+						var minTempo3 = data.daily.data[3].temperatureMin.toFixed(0);
+						var sumaRy3 = data.daily.data[3].summary;
+
+						if (icon3 == "clear-day") {
+							var skycons = new Skycons({ "color": "Yellow" });
+							skycons.add("dicon3", Skycons.CLEAR_DAY);
+							skycons.play();
+						}
+						if (icon3 == "clear-night") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon3", Skycons.CLEAR_NIGHT);
+							skycons.play();
+						}
+						if (icon3 == "partly-cloudy-day") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon3", Skycons.PARTLY_CLOUDY_DAY);
+							skycons.play();
+						}
+						if (icon3 == "partly-cloudy-night") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon3", Skycons.PARTLY_CLOUDY_NIGHT);
+							skycons.play();
+						}
+						if (icon3 == "cloudy") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon3", Skycons.CLOUDY);
+							skycons.play();
+						}
+						if (icon3 == "rain") {
+							var skycons = new Skycons({ "color": "blue" });
+							skycons.add("dicon3", Skycons.RAIN);
+							skycons.play();
+						}
+						if (icon3 == "sleet") {
+							var skycons = new Skycons({ "color": "blue" });
+							skycons.add("dicon3", Skycons.SLEET);
+							skycons.play();
+						}
+						if (icon3 == "snow") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon3", Skycons.SNOW);
+							skycons.play();
+						}
+						if (icon3 == "wind") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon3", Skycons.WIND);
+							skycons.play();
+						}
+						if (icon3 == "fog") {
+							var skycons = new Skycons({ "color": "white" });
+							skycons.add("dicon3", Skycons.FOG);
+							skycons.play();
+						}
+						$('#dicon3').html(icon3);
+						$('#dmTempo3').html('Max ' + mTempo3 + '&#8451');
+						$('#dminTempo3').html('Min ' + minTempo3 + '&#8451');
+						$('#dsumaRy3').html(sumaRy3);
+					}
+					twoAfterTommorow();
 					var loK = data.timezone;
 					var cityLok = loK.substring(loK.lastIndexOf("/") + 1);
 					var wIcon = data.currently.icon;
+					//.........................
+					
+
 					//	.locationstring.split('/').pop();
-					$('#currentTemp').html(data.currently.temperature.toFixed(0)+'<span id="cantigrader">c</span>');
+					$('#currentTemp').html(data.currently.temperature.toFixed(0) +'<span id="cantigrader">&#8451</span>');
 					$('#toDay').html(day);
+					$('#toMar').html(etomarow);
+					$('#otoMar').html(eoTomarow);
+					$('#totoMar').html(etwoTomarow);
+					$('#tretoMar').html(eTreetomarow);
+
+					
 					$('#yourLocation').html(cityLok);
 					$('#weatherIcon').html(wIcon);
-					$('#windSpeed').html('windSpeed ' + data.currently.windSpeed);
+					$('#windSpeed').html("wind: " + data.currently.windSpeed.toFixed(2));
 					$('#weatherHumiditi').html('Humiditi ' + data.currently.humidity);
 					$('#summaryToday').html(data.currently.summary);
+				
 					if (wIcon == "clear-day") {
 						var skycons = new Skycons({ "color": "Yellow" });
 						skycons.add("weatherIcon", Skycons.CLEAR_DAY);
@@ -134,6 +392,24 @@ getLatitudeLongitude(showResult);
 		});
 		//moment script
 		var day = moment().format('dddd');
+
+		var tomarow = moment().add(1, 'days').calendar(); 
+        // remove evrerything after "speace"
+		var etomarow = tomarow.substring(0, tomarow.indexOf(' '));
+
+		var oneTomarow = moment().add(2, 'days').calendar();
+		// remove evrerything after "speace"
+		var eoTomarow = oneTomarow.substring(0, oneTomarow.indexOf(' '));
+
+		var twoTomarow = moment().add(3, 'days').calendar();
+		// remove evrerything after "speace"
+		var etwoTomarow = twoTomarow.substring(0, twoTomarow.indexOf(' '));
+
+		var treeTomarow = moment().add(4, 'days').calendar();
+		// remove evrerything after "speace"
+		var eTreetomarow = treeTomarow.substring(0, treeTomarow.indexOf(' '));
+
+
 		function displayTime() {
 			var time = moment().format(' HH:mm:ss');
 			$('#timeNow').text(time);
